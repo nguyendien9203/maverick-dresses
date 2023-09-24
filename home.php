b4d3bf7edbc44e0f8f3935a7f1540bb762f3c4da
trungsontiet <tiettrungson@gmail.com>
2023-09-25 04:16:12 +0700

    .

--- home.php
+++ home.php
@@ -3,9 +3,436 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/yourcode.js"></script>
     <title>Document</title>
+
 </head>
 <body>
-    Hello world
+    <header>
+        <div class="logo">
+            <b>Marverick</b>
+            
+        </div>
+        <div class="menu">
+            <li><a href="">Trang chủ</a></li>
+            <li><a href="">Sản Phẩm</a> 
+                <ul class="Sub-menu">
+                    <li><a href="">Áo</a>
+                        <ul>
+                            <li><a href="">Tất cả áo</a></li>
+                            <li><a href="">Áo sơ mi</a></li>
+                            <li><a href="">Áo thun</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="">Váy</a></li>
+                    <li><a href="">Logo</a></li>
+                    <li><a href="">Chân váy</a></li>
+                    <li><a href="">Quần</a>  
+                        <ul>
+                            <li><a href="">Tất cả quần</a></li>
+                            <li><a href="">Quần short</a></li>
+                            <li><a href="">Quần thể thao</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="">Phụ Kiện</a>
+                        <ul>
+                            <li><a href="">Tất cả phụ kiện</a></li>
+                            <li><a href="">Thắt lưng</a></li>
+                            <li><a href="">Cà vạt</a></li>
+                            <li><a href="">Tất</a></li>
+                        </ul>
+                    </li>
+                </ul>                  
+            </li>
+            <li><a href="">Về Chúng Tôi</a></li>
+            <li><a href="">Liên Hệ</a></li>
+        </div>
+        <div class="user-controls">
+            <button class="login-button">Đăng nhập</button>
+            <button class="login-button">Đăng kí</button>
+        </div>
+    </header>
+    <section id="banner"> 
+        <div class="aspect-radio-169">
+            <img src="./res4e8d6ec149bd27484e5a9168bdad547ffr.jpg" alt="">
+            <img src="./res5018c211e15dceec7b45313103ded10efr.jpg" alt="">
+            <img src="./res736df3c8b87140c5787d1a36dafbf11efr.jpg" alt="">
+            <img src="./resa30962e463510576d369e33068f20ca3fr.jpg" alt="">
+            <img src="./resbe4705e38db80081e8ca4b3891f8e7fefr.jpg" alt="">
+        </div>
+        <div class="dot-container">
+            <div class="dot active"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+            <div class="dot"></div>
+        </div>
+    </section>
+    <div id="wp_product">
+        <h2>Danh mục sản phẩm</h2>
+        <ul id="list_product">  
+            <li class="product">
+                <img src="./áo.jpg" alt="">
+                <div class="name_product">Áo</div>
+            </li>
+            <li class="product">
+                <img src="./váy.jpg" alt="">
+                <div class="name_product">Váy</div>
+            </li>
+            <li class="product">
+                <img src="./chan vay.avif" alt="">
+                <div class="name_product">Chân váy</div>
+            </li>
+            <li class="product">
+                <img src="./logo.png" alt="">
+                <div class="name_product">Logo</div>
+            </li>
+            <li class="product">
+                <img src="./quan.jpg" alt="">
+                <div class="name_product">Quần</div>
+            </li>
+            <li class="product">
+                <img src="./Phụ kiên.jpg" alt="">
+                <div class="name_product">Phụ Kiện</div>
+            </li>
+            <li class="product">
+                <img src="./test2.jpg" alt="">
+                <div class="name_product">Phụ Kiện</div>
+            </li>
+            <li class="product">
+                <img src="./test1.jpg" alt="">
+                <div class="name_product">Phụ Kiện</div>
+            </li>
+        </ul>
+    </div>
+    <div id="wp_product-arrival">
+        <h2>Hàng mới về</h2>
+        <ul id="list_product-arrival">  
+            <li class="new-product">
+                <img src="./áo.jpg" alt="">
+                <div class="gallery">
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span><span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                </div>
+                <div class="size-product">
+                    <span>S</span>
+                    <span>M</span>
+                    <span>L</span>
+                    <span>XL</span>
+                </div>
+                <div class="name_product-arrival">Áo</div>
+                <div class="price">100.000 VND</div>
+            </li>
+            <li class="new-product">
+                <img src="./áo.jpg" alt="">
+                <div class="gallery">
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span><span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                </div>
+                <div class="size-product">
+                    <span>S</span>
+                    <span>M</span>
+                    <span>L</span>
+                    <span>XL</span>
+                </div>
+                <div class="name_product-arrival">Áo</div>
+                <div class="price">100.000 VNĐ</div>
+            </li>
+            <li class="new-product">
+                <img src="./áo.jpg" alt="">
+                <div class="gallery">
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span><span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                </div>
+                <div class="size-product">
+                    <span>S</span>
+                    <span>M</span>
+                    <span>L</span>
+                    <span>XL</span>
+                </div>
+                <div class="name_product-arrival">Áo</div>
+                <div class="price">100.000 VNĐ </div>
+            </li>
+            <li class="new-product">
+                <img src="./áo.jpg" alt="">
+                <div class="gallery">
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span><span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                </div>
+                <div class="size-product">
+                    <span>S</span>
+                    <span>M</span>
+                    <span>L</span>
+                    <span>XL</span>
+                </div>
+                <div class="name_product-arrival">Áo</div>
+                <div class="price">100.000 VNĐ</div>
+            </li>
+        </ul>
+    </div>
+    <div id="wp_product-hot">
+        <h2>SẢN PHẨM NỔI BẬT</h2>
+        <ul id="list_product-hot">  
+            <li class="hot-product">
+                <img src="./áo.jpg" alt="">
+                <div class="gallery">
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span><span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                </div>
+                <div class="size-product">
+                    <span>S</span>
+                    <span>M</span>
+                    <span>L</span>
+                    <span>XL</span>
+                </div>
+                <div class="name_product-hot">Áo</div>
+                <div class="price">100.000 VND</div>
+            </li>
+            <li class="hot-product">
+                <img src="./áo.jpg" alt="">
+                <div class="gallery">
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span><span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                </div>
+                <div class="size-product">
+                    <span>S</span>
+                    <span>M</span>
+                    <span>L</span>
+                    <span>XL</span>
+                </div>
+                <div class="name_product-hot">Áo</div>
+                <div class="price">100.000 VNĐ</div>
+            </li>
+            <li class="hot-product">
+                <img src="./áo.jpg" alt="">
+                <div class="gallery">
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span><span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                </div>
+                <div class="size-product">
+                    <span>S</span>
+                    <span>M</span>
+                    <span>L</span>
+                    <span>XL</span>
+                </div>
+                <div class="name_product-hot">Áo</div>
+                <div class="price">100.000 VNĐ </div>
+            </li>
+            <li class="hot-product">
+                <img src="./áo.jpg" alt="">
+                <div class="gallery">
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                    <span>
+                        <img src="./áo.jpg" alt="">
+                    </span><span>
+                        <img src="./áo.jpg" alt="">
+                    </span>
+                </div>
+                <div class="size-product">
+                    <span>S</span>
+                    <span>M</span>
+                    <span>L</span>
+                    <span>XL</span>
+                </div>
+                <div class="name_product-hot">Áo</div>
+                <div class="price">100.000 VNĐ</div>
+                <li class="hot-product">
+                    <img src="./áo.jpg" alt="">
+                    <div class="gallery">
+                        <span>
+                            <img src="./áo.jpg" alt="">
+                        </span>
+                        <span>
+                            <img src="./áo.jpg" alt="">
+                        </span><span>
+                            <img src="./áo.jpg" alt="">
+                        </span>
+                    </div>
+                    <div class="size-product">
+                        <span>S</span>
+                        <span>M</span>
+                        <span>L</span>
+                        <span>XL</span>
+                    </div>
+                    <div class="name_product-hot">Áo</div>
+                    <div class="price">100.000 VNĐ</div>
+                    <li class="hot-product">
+                        <img src="./áo.jpg" alt="">
+                        <div class="gallery">
+                            <span>
+                                <img src="./áo.jpg" alt="">
+                            </span>
+                            <span>
+                                <img src="./áo.jpg" alt="">
+                            </span><span>
+                                <img src="./áo.jpg" alt="">
+                            </span>
+                        </div>
+                        <div class="size-product">
+                            <span>S</span>
+                            <span>M</span>
+                            <span>L</span>
+                            <span>XL</span>
+                        </div>
+                        <div class="name_product-hot">Áo</div>
+                        <div class="price">100.000 VNĐ</div>
+                        <li class="hot-product">
+                            <img src="./áo.jpg" alt="">
+                            <div class="gallery">
+                                <span>
+                                    <img src="./áo.jpg" alt="">
+                                </span>
+                                <span>
+                                    <img src="./áo.jpg" alt="">
+                                </span><span>
+                                    <img src="./áo.jpg" alt="">
+                                </span>
+                            </div>
+                            <div class="size-product">
+                                <span>S</span>
+                                <span>M</span>
+                                <span>L</span>
+                                <span>XL</span>
+                            </div>
+                            <div class="name_product-hot">Áo</div>
+                            <div class="price">100.000 VNĐ</div>
+                            <li class="hot-product">
+                                <img src="./áo.jpg" alt="">
+                                <div class="gallery">
+                                    <span>
+                                        <img src="./áo.jpg" alt="">
+                                    </span>
+                                    <span>
+                                        <img src="./áo.jpg" alt="">
+                                    </span><span>
+                                        <img src="./áo.jpg" alt="">
+                                    </span>
+                                </div>
+                                <div class="size-product">
+                                    <span>S</span>
+                                    <span>M</span>
+                                    <span>L</span>
+                                    <span>XL</span>
+                                </div>
+                                <div class="name_product-hot">Áo</div>
+                                <div class="price">100.000 VNĐ</div>
+            </li>
+        </ul>
+    </div>
+    <div class="contact-box">
+        <h2>Liên hệ</h2>
+        <form action="submit.php" method="post">
+            <label for="name">Họ và tên :</label>
+            <input type="text" id="name" name="name" required>
+
+            <label for="email">Email:</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="message">Phản hồi:</label>
+            <textarea id="message" name="message" rows="4" required></textarea>
+
+            <input type="submit" value="Gửi">
+        </form>
+    </div>
+    <footer>
+        <div class="contact">
+            <h3>Liên hệ với chúng tôi</h3>
+            <p>Công ty TNHH APTECH</p>
+            <p>Address: 285 Đội Cấn, Ba Đình, Hà Nội</p>
+            <p>Phone: +1 (123) 456-7890</p>
+            <p>Email: APTECH@GMAIL.COM</p>    
+        </div>
+        <div class="follow">
+            <h3>Follow Us</h3>
+            <a href="#">Facebook</a>
+            <a href="#">Twitter</a>
+            <a href="#">Instagram</a>
+        </div>
+        </div>  
+        <div class="map">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d119143.37967605892!2d105.75009557070132!3d21.063449532496147!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3135ab0d6e603741%3A0x208a848932ac2109!2sAptech%20Computer%20Education!5e0!3m2!1sen!2s!4v1695585652453!5m2!1sen!2s" width="100%" height="300px" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+    </footer>
+</div>
+<br>
+<div class="copyright">
+    &copy; 2023 Your Company Name. All Rights Reserved.
+</div>
 </body>
+<script>
+    var imgPosition = document.querySelectorAll(".aspect-radio-169 img");
+    var imgContainer = document.querySelector('.aspect-radio-169');
+    var dotItems = document.querySelectorAll(".dot");
+
+    let index = 0;
+    let imgNumber = imgPosition.length;
+
+    imgPosition.forEach(function(image, i) {
+        image.style.left = i * 100 + "%";
+        dotItems[i].addEventListener("click", function() {
+            dotSlide(i);
+        });
+    });
+
+    function imgBanner() {
+        index++;
+        if (index >= imgNumber) {
+            index = 0;
+        }
+        dotSlide(index);
+    }
+
+    function dotSlide(i) {
+        var dotActive = document.querySelector(".active");
+        dotActive.classList.remove("active");
+        dotItems[i].classList.add("active");
+        imgContainer.style.left = "-" + i * 100 + "%";
+    }
+
+    setInterval(imgBanner, 3000);
+</script>
 </html>
