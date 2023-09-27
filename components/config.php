8b456c79afc82287f322c46d9791ba3a6df9968e
nguyendien092 <ndien9203@gmail.com>
2023-09-27 17:26:19 +0700

    .

--- components/config.php
+++
@@ -1,3 +0,0 @@
-<?php
-define("BASE_URL", "http://localhost/project/eProject/Maverick_Dresses");
-?>
--- inc/header.php
+++ inc/header.php
@@ -1,6 +1,6 @@
-<?php
-require_once("./components/config.php");
-?>
+
+<?php require_once("../config/config.php"); ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
