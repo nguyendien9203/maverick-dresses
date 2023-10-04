fa1c24bc019cdbfece9ee5a9ede901caf5aa6b61
trungsontiet <tiettrungson@gmail.com>
2023-10-05 02:24:44 +0700

    Update about.php

--- about.php
+++ about.php
@@ -1,6 +1,33 @@
 <?php require_once("./inc/header.php");?>
-đây là about us!!!òhsdifhsd
-sdfsdfsdf
-sdfsdfsdfsdfs
-
+<section class="about-us">
+        <div class="content-top">
+            <h1>Về Chúng Tôi</h1>
+        </div>
+        <div class="container-content">
+        <div class="content-left">
+            <img src="./ca39c1ba-7565-4ef8-b16c-b9793b4b91ed_1200x630_stretched.jpeg" alt="">
+        </div>
+        <div class="content-right">
+            <div class="content-box">
+                <h1>Đôi nét về chúng tôi</h1>
+                <p>Chào mừng bạn đến với Maverick Dress - nơi nâng cao phong cách và tự tin của bạn thông qua thiết kế đồng phục độc đáo và chất lượng. Tại Maverick Dress, chúng tôi không chỉ sản xuất đồng phục, mà chúng tôi còn tạo ra những câu chuyện, kết nối người mặc với thế giới xung quanh và làm cho mỗi bước đi của bạn trở nên đặc biệt.</p>
+            
+                <h2>Sứ mệnh của chúng tôi:</h2>
+                <p>Maverick Dress không chỉ là một công ty đồng phục, mà là một tinh thần sáng tạo, nơi mỗi chiếc áo đều là một tác phẩm nghệ thuật. Chúng tôi cam kết mang lại sự tự tin và hạnh phúc cho khách hàng thông qua việc thiết kế và sản xuất đồng phục tinh tế và chất lượng cao.</p>
+            
+                <h2> Dịch vụ của chúng tôi:</h2>
+                <ol>
+                    <li><strong>Thiết kế Độc Đáo:</strong> Chúng tôi có một đội ngũ thiết kế tài năng, luôn sẵn lòng lắng nghe ý tưởng của bạn và biến chúng thành những mẫu đồng phục độc đáo và ấn tượng.</li>
+                    <li><strong>Chất Lượng Đỉnh Cao:</strong> Chúng tôi sử dụng nguyên liệu chất lượng cao và tiên tiến nhất để đảm bảo rằng mỗi chiếc đồng phục không chỉ đẹp mắt mà còn thoải mái và bền bỉ.</li>
+                    <li><strong>Giao Hàng Đúng Hẹn:</strong> Chúng tôi hiểu rằng thời gian quan trọng đối với bạn, vì vậy chúng tôi cam kết giao sản phẩm đúng hẹn và theo đúng tiêu chuẩn đã thảo luận.</li>
+                    <li><strong>Dịch Vụ Khách Hàng Tận Tâm:</strong> Đội ngũ hỗ trợ của chúng tôi luôn sẵn lòng giúp đỡ bạn qua mọi bước đường, từ việc chọn mẫu đến quy trình đặt hàng và sau khi giao hàng.</li>
+                </ol>
+            
+                <h2>Chúng tôi Đặt Niềm Tin vào Bạn:</h2>
+                <p>Tại Maverick Dress, chúng tôi tin rằng mỗi người đều có một phong cách riêng và chúng tôi muốn giúp bạn thể hiện điều đó thông qua đồng phục của mình...</p>
+            </div>
+            
+        </div>
+       </div>
+    </section>
 <?php require_once("./inc/footer.php")?>
